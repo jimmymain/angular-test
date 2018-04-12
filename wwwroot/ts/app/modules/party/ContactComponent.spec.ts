2a539a01cc382d3881c7c3310092747df156fbc9
Craig Main <craig@palantir.co.za>
2018-04-12 14:05:35 +0200

    attempt at providing resources, and compiling the components

--- wwwroot/ts/app/modules/party/ContactComponent.spec.ts
+++ wwwroot/ts/app/modules/party/ContactComponent.spec.ts
@@ -2,21 +2,35 @@
 import { TestBed } from "@angular/core/testing";
 import { ContactComponent } from "./ContactComponent";
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+import { ApplicationSettings } from "../../components/ApplicationSettings";
+import { PartyService } from "../../lib/party/PartyService";
+import { ExpiryService } from "../../lib/ExpiryService";
+import { RoleTypeProvider } from "../../../domain/Domain/RoleType";
+import { MessageDisplayService } from "../../lib/MessageDisplayService";
+import { NGXLogger } from "ngx-logger";
 
 describe("contact component",
 	() => {
-
+		var contactComponent: ContactComponent;
 		beforeEach(() => {
 			TestBed.configureTestingModule({
-				providers: [
-					ContactComponent
-				]
-			});
+			providers: [
+					{provide: ApplicationSettings, useValue: {}},
+					{provide: PartyService, useValue: {}},
+					{provide: ExpiryService, useValue: {}},
+					{provide: RoleTypeProvider, useValue: {}},
+					{provide: MessageDisplayService, useValue: {}},
+					{provide: NGXLogger, useValue: {}},
+				],				
+				declarations: [ContactComponent]
+			}).compileComponents();
+			// create
+			var contactComponent = TestBed.get(ContactComponent);
+
 		});
 
 		it("save should save something",
 			() => {
-				var contactComponent = TestBed.get(ContactComponent);
 				contactComponent.save();
 			});
 
